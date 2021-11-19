f3df1d84e6c4768777f9bc355d56ed4a50abc20e
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2021-11-19 15:10:12 +0200

    Part 4 Tasks 4.1-4.2 return

---
+++ Part_4/README.md
@@ -0,0 +1,4 @@
+# Osa 4 - Express-sovellusten testaaminen, käyttäjänhallinta
+
+### Sovelluksen rakenne ja testauksen alkeet
+  * [Tehtävät 4.1 - 4.2](https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#tehtavat-4-1-4-2) - ([blogilista-backend](https://github.com/j-pietila/FullStackOpen-2021/tree/main/Part_4/blogilista-backend))
---
+++ Part_4/blogilista-backend/app.js
@@ -0,0 +1,29 @@
+const config = require("./utils/config")
+const express = require("express")
+const cors = require("cors")
+const logger = require("./utils/logger")
+const middleware = require("./utils/middleware")
+const mongoose = require("mongoose")
+const blogsRouter = require("./controllers/blogs")
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB")
+  })
+  .catch(error => {
+    logger.error("Error connecting to MongoDB:", error.message)
+  })
+
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
+app.use(middleware.morganLogger)
+
+app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
